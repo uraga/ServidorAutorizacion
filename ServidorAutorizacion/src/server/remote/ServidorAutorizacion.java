3f5ee51546b1921f80961dd367146a201880a583
Jon Uraga <j.uraga@opendeusto.es>
2020-01-18 12:04:56 +0100

    login perfecto, registro no da problemas pero no se guarda en arraylist.

--- ServidorAutorizacion/src/server/remote/ServidorAutorizacion.java
+++ ServidorAutorizacion/src/server/remote/ServidorAutorizacion.java
@@ -13,30 +13,52 @@ public class ServidorAutorizacion extends UnicastRemoteObject implements IServid
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
 	protected ServidorAutorizacion() throws RemoteException {
 		super();
+		Usuario u1 = new Usuario();
+		u1.setEmail("j.uraga@opendeusto.es"); u1.setContrasenya("12345");
+		Usuario u2 = new Usuario();
+		u2.setEmail("gorka.garate@opendeusto.es"); u2.setContrasenya("abcd");
+		Usuario u3 = new Usuario();
+		u3.setEmail("ibai.guillen@opendeusto.es"); u3.setContrasenya("12345");
+		Usuario u4 = new Usuario();
+		u4.setEmail("yeray.bellanco@opendeusto.es"); u4.setContrasenya("ab");
+		
+		usuarios.add(u1);
+		usuarios.add(u2);
+		usuarios.add(u3);
+		usuarios.add(u4);
 	}
-
-	private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 	
 	@Override
 	public void registrar(String email, String contrasenya) {
+		System.out.println("..");
+		System.out.println("..Mail: " + email);
+		System.out.println("..Pass: " + contrasenya);
+		boolean existe = false;
 		for (Usuario u : usuarios) {
 			if (u.getEmail().equals(email)) {
+				existe = true;
 				System.out.println("Error -- ya existe un usuario con ese email");
-			} else {
-				Usuario user = new Usuario();
-				user.setEmail(email);
-				user.setContrasenya(contrasenya);
-				usuarios.add(user);
-				System.out.println("Usuario registrado correctamente");
 			}
-		}		
+		}
+		if (!existe) {
+			Usuario u = new Usuario();
+			u.setEmail(email);
+			u.setContrasenya(contrasenya);
+			usuarios.add(u);
+			System.out.println("Usuario registrado correctamente");
+		}
+		
 	}
 
 	@Override
 	public boolean login(String email, String contrasenya) {
+		System.out.println("..");
+		System.out.println("..Mail: " + email);
+		System.out.println("..Pass: " + contrasenya);
 		for (Usuario u : usuarios) {
 			if (u.getEmail().equals(email)) {
 				if (u.getContrasenya().equals(contrasenya)) {
@@ -70,6 +92,13 @@ public class ServidorAutorizacion extends UnicastRemoteObject implements IServid
 			IServidorAutorizacion server = new ServidorAutorizacion();
 			Naming.rebind(name, server);
 			System.out.println("* Server '" + name + "' active and waiting...");
+			
+			server.registrar("prueba@prueba.es", "prueba"); //Se registra bien
+			server.login("prueba@prueba.es", "prueba"); // ¿¿Login correcto??
+			server.registrar("j.uraga@opendeusto.es", "12345"); //Ya existe
+			server.login("j.uraga@opendeusto.es", "12345"); //Login correcto
+			server.login("j.uraga@opendeusto.com", "12345"); //Email no existe 
+			server.login("j.uraga@opendeusto.es", "abcd"); //Contraseña incorrecta
 		} catch (Exception e) {
 			System.err.println("- Exception running the server: " + e.getMessage());
 			e.printStackTrace();
